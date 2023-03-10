5887a54743ee033018580552a9a5a3612bca7ac6
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-03-10 20:44:35 +0800

    > make 211250127 root Linux LAPTOP-R867NURB 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:44:35 up 13:09,  0 users,  load average: 0.04, 0.01, 0.00 d6cf20b45fc37a27dae5a919bd1bbf84a9be54f

--- src/Main.java
+++ src/Main.java
@@ -20,15 +20,22 @@ public class Main {
 
         List<? extends Token> tokens = sysYLexer.getAllTokens();
         if (!myListener.status) {
-            for (int i = 0; i < tokens.size(); i++) {
-                Token token = tokens.get(i);
+            for (Token token : tokens) {
                 String text = token.getText();
                 String type = SysYLexer.ruleNames[token.getType() - 1];
-                if (type == "INTEGER_CONST" && text.length()>1){
-                    if (text.charAt(1)=='x' || text.charAt(1)=='X')
+//                if (type == "INTEGER_CONST" && text.length()>1){
+//                    if (text.charAt(1)=='x' || text.charAt(1)=='X')
+//                        text = String.valueOf((Integer.parseInt(text.substring(2),16)));
+//                    else if (text.charAt(0) == '0')
+//                        text = String.valueOf((Integer.parseInt(text,8)));
+//                }
+                if(type == "INTEGER_CONST"){
+                    if (text.length()>1 && (text.charAt(1)=='x'||text.charAt(1)=='X'){
                         text = String.valueOf((Integer.parseInt(text.substring(2),16)));
-                    else if (text.charAt(0) == '0')
-                        text = String.valueOf((Integer.parseInt(text,8)));
+                    }
+                    else if (text.charAt(0) == '0' && text.length()>1){
+                        text = String.valueOf((Integer.parseInt(text.substring(1),8)));
+                    }
                 }
 
                 System.err.println(type + " " + text + " at Line " + token.getLine() + ".");
