2cb4d45d85e17f095181c510c1206af2689d1c3f
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-03-10 19:21:20 +0800

    > make 211250127 root Linux LAPTOP-R867NURB 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:21:20 up 11:46,  0 users,  load average: 0.09, 0.09, 0.10 2717bcff521e644f1104c576e40c08bdf9b6c397

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
                 String text = token.getText();
                 String type = SysYLexer.ruleNames[token.getType() - 1];
                 if (type == "INTEGER_CONST"){
-                    type = (String.valueOf(type)+0).toString();
+                    text = (String.valueOf(text)+0).toString();
                 }
 
                 System.out.println(type + " " + text + " at Line " + token.getLine() + ".");
