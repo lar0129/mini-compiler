e864cf1caacc54bb8dcf9e33070ad41642f5a1d6
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-03-10 18:24:05 +0800

    > make 211250127 root Linux LAPTOP-R867NURB 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:24:05 up 10:48,  0 users,  load average: 0.00, 0.00, 0.00 4d910b6e417636398983c03a832b031d4987d190

--- src/myErrorListener.java
+++ src/myErrorListener.java
@@ -1,9 +1,14 @@
 import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 
 public class myErrorListener extends BaseErrorListener {
     public myErrorListener() {
-        System.out.println("Error type A at Line 3: Mysterious character \"~\".");
+        //System.out.println("Error type A at Line 3: Mysterious character \"~\".");
     }
 
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        System.out.println("Error type A at Line " + line + ":" + msg);
+    }
 
 }
