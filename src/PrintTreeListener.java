f3cef53f6088be7f7e2d4fd4229ffffdaece1ea1
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-04-06 20:22:49 +0800

    > make 211250127 root Linux LAPTOP-R867NURB 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:22:49 up  6:43,  0 users,  load average: 0.01, 0.05, 0.01 8b099ee79f0493cf164d35028ae1d2d77b6ee3

--- src/PrintTreeListener.java
+++ src/PrintTreeListener.java
@@ -1,27 +1,34 @@
 
 
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class PrintTreeListener  extends SysYParserBaseListener {
         String currentFunctionName = null;
-
-        int deepIdx = 0;
-        int temp = 0;
 
         String[] ruleNames = SysYParser.ruleNames;
 
         @Override
         public void enterEveryRule(ParserRuleContext ctx) {
-//                deepIdx = ctx.depth();
+                int deepIdx = ctx.depth();
 //                System.out.println(deepIdx);
-                temp = ctx.getRuleIndex();
+                int ruleIdx = ctx.getRuleIndex();
+//                System.out.println(ruleIdx);
+                String temp = "";
+                for (int i=1;i<deepIdx;i++){
+                        temp+="  ";
+                }
+                temp+=ruleNames[ruleIdx];
                 System.out.println(temp);
         }
 
-        @Override
-        public void exitEveryRule(ParserRuleContext ctx) {
-//            String callee = ctx.ID().getText();
-//            graph.addEdge(currentFunctionName, callee);
+        @Override public void visitTerminal(TerminalNode node) {
+
+        }
+
+        @Override public void visitErrorNode(ErrorNode node) {
+
         }
 
 
