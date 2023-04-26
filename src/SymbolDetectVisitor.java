b1d43693f05cc74d4c2dbd77e35d36c145997c0a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-04-26 19:18:22 +0800

    > make 211250127 lar Linux LAPTOP-R867NURB 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:18:22 up  8:43,  0 users,  load average: 0.10, 0.10, 0.06 2dceb69d953b628f7c0ae3f9a5092fdfaa2837c2

--- src/SymbolDetectVisitor.java
+++ src/SymbolDetectVisitor.java
@@ -32,7 +32,6 @@ public class SymbolDetectVisitor extends SysYParserBaseVisitor<Void>{
         String funName = ctx.IDENT().getText();
         FunctionSymbol funcSymbolInTable = (FunctionSymbol)globalScope.resolve(funName);
 
-        System.out.println(currentScope.getName());
         // 报告 Error type 4 函数重复定义
         if(funcSymbolInTable != null){
             errorTable.addErrorTable(getLineNo(ctx),4);
@@ -45,6 +44,7 @@ public class SymbolDetectVisitor extends SysYParserBaseVisitor<Void>{
         currentScope.define(fun);
         currentScope = fun;
 
+        System.out.println(currentScope.getName());
         // 遍历子树
         Void ret = super.visitFuncDef(ctx);
         // 回到上一层 Scope
