a8810cb034027047cc476cf21f3472ce0104dec0
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-04-26 19:06:49 +0800

    > make 211250127 lar Linux LAPTOP-R867NURB 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:06:49 up  8:31,  0 users,  load average: 0.04, 0.02, 0.00 6d67a32b7c086719dd94b6d5cc389566abb4f1

--- src/SymbolDetectVisitor.java
+++ src/SymbolDetectVisitor.java
@@ -30,11 +30,11 @@ public class SymbolDetectVisitor extends SysYParserBaseVisitor<Void>{
     public Void visitFuncDef(SysYParser.FuncDefContext ctx) {
         String typeName = ctx.funcType().getText();
         String funName = ctx.IDENT().getText();
-        FunctionSymbol funcSymbolInTable = (FunctionSymbol)globalScope.resolve(funName);
+        FunctionSymbol funcSymbolInTable = (FunctionSymbol)currentScope.resolve(funName);
 
+        System.out.println(currentScope.getName());
         // 报告 Error type 4 函数重复定义
         if(funcSymbolInTable != null){
-            System.out.println(currentScope.getName());
             errorTable.addErrorTable(getLineNo(ctx),4);
             funName = funName + getLineNo(ctx);
         }
@@ -49,6 +49,7 @@ public class SymbolDetectVisitor extends SysYParserBaseVisitor<Void>{
         Void ret = super.visitFuncDef(ctx);
         // 回到上一层 Scope
         currentScope = currentScope.getEnclosingScope();
+        System.out.println(currentScope.getName());
         return ret;
     }
 
